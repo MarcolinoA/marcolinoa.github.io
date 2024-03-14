55858fda894ca4e77154ae0da2d3e838d7632030
MarcolinoA <marcolinonft@gmail.com>
2024-03-14 18:12:28 +0100

    ho cercato di sistemare la parte dell'immagine ma non sono sicuro che mi piaccia

--- portfolio/src/components/Description/Description.jsx
+++ portfolio/src/components/Description/Description.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import image from "./coachProva.jpg";
+import coding_image from "./coding.jpg";
 import "./Description.css";
 import GithubIcon from "../Icons/GithubIcon";
 import IgIcon from "../Icons/IgIcon";
@@ -7,25 +8,31 @@ import LinkedinIcon from "../Icons/LinkedinIcon";
 
 const Description = () => {
   return (
-    <div className="description-container">
-      <div className="first-section">
-        <img src={image} alt="" className="image" />
-        <h2 className="title">
-          Junior <br /> Frontend <br /> Developer
-        </h2>
-      </div>
-      <div className="second-section">
-        <p className="desc-content">
-          Ciao! Sono Marco, un Junior Frontend Developer, il coding mi
-          appassiona e mi diverte. Ho una solida base in HTML, CSS e JavaScript
-          e React. Il mio viaggio è iniziato da poco e non ho intenzione di
-          fermarmi allo sviluppo frontend.
-        </p>
-        <div className="icons-container">
-          <GithubIcon className={"icon"} />
-          <IgIcon className={"icon"} />
-          <LinkedinIcon className={"icon"} />
+    <div className="home-container">
+      <div className="description-container">
+        <div className="first-section">
+          <img src={image} alt="" className="my-image" />
+          <h2 className="title">
+            Junior <br /> Frontend <br /> Developer
+          </h2>
         </div>
+        <div className="second-section">
+          <p className="desc-content">
+            Ciao! Sono Marco, un Junior Frontend Developer, il coding mi
+            appassiona e mi diverte. Ho una solida base in HTML, CSS e
+            JavaScript e React. Il mio viaggio è iniziato da poco e non ho
+            intenzione di fermarmi allo sviluppo frontend.
+          </p>
+          <div className="icons-container">
+            <GithubIcon className={"icon"} />
+            <IgIcon className={"icon"} />
+            <LinkedinIcon className={"icon"} />
+          </div>
+        </div>
+      </div>
+
+      <div className="image-container">
+        <img src={coding_image} alt="" className="coding-img"/>
       </div>
     </div>
   );
